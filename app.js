167628707dc03080ddef90cd825715c00c748e3e
ilanser <ilanserpro@gmail.com>
2018-05-20 11:57:32 +0300

    slow start

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log('Example app listening on port ' + port);
 });
