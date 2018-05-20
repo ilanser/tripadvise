ae58d8539c2ed10254194ff2bd5f17168900215b
ilanser <ilanserpro@gmail.com>
2018-05-20 17:32:28 +0300

    slow start

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,24 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/login', function(req,res){
+ //TODO check if user in DB and connect him + return token
+});
+
+router.post('/register', function(req,res){
+   //TODO read the body and register the user and return response
+});
+
+router.route('/favorites')
+    .get(function(req,res){
+
+    })
+    .put(function(req,res){
+
+    });
+
+router.post('/retrievepassword',function(req,res){
+    //TODO retrieve password function will send user information from
+    // DB with question and answer
+    });
 module.exports = router;
